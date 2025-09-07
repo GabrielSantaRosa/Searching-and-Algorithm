6b042663b8a2a44f2f4f6a72e8cf9b180b821f8f
GabrielSantaRosa <gabrielsanta279@gmail.com>
2025-09-07 17:57:56 -0300

    testes

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@ int main()
 {
     std::string palavra{"Oi"};
     std::cout << palavra.length() << "\n";
-    
+    while (true) 
+    {
+        if(palavra.length() == 2)
+        {
+            break;
+        }        
+    }
+
     return 0;
 }
